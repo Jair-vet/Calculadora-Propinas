4e3f1fee68045c7a94f3c508c04397ff622cf18b
Jair <jairaceves56@gmail.com>
2024-03-19 16:54:46 -0600

    Creamos una funcion para comunicar el componente y al dar click agregar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,38 @@
+import { MenuItems } from "./components/MenuItems";
+import { menuItems } from './data/db';
+import useOrder from "./hooks/useOrder";
 
 
 function App() {
+  
+  const { addItem } = useOrder()
 
   return (
     <>
-      <h1>Hola Mundo</h1>
+      <header className="bg-sky-600 py-5">
+        <h1 className="text-center text-4xl font-black text-white">Calculadora de Proinas y Consumos</h1>
+      </header>
+
+      <main className="max-w-7xl md:mx-auto py-20 mx-4 md:grid md:grid-cols-2">
+        <div>
+          <h2 className="text-3xl font-black text-center">Menú</h2>
+          
+          <div className="mt-10">
+            { menuItems.map(item => (
+              <MenuItems
+                key={item.id}
+                item={item}
+                addItem={addItem}
+              />
+            ))}
+          </div>
+        </div>
+
+        <div>
+          <h2>Consumo</h2>
+        </div>
+      </main>
+
     </>
   )
 }
--- src/components/MenuItems.tsx
+++ src/components/MenuItems.tsx
@@ -2,14 +2,16 @@ import type { MenuItem } from "../types"
 
 
 type MenuItemProps = {
-    item: MenuItem
+    item: MenuItem,
+    addItem: () => void
 }
 
-export const MenuItems = ({item} :  MenuItemProps) => {
+export const MenuItems = ({item, addItem} :  MenuItemProps) => {
   return (
     <button
-        className="bg-sky-200 first-of-type:rounded-t-xl border-2 border-sky-200 w-full p-3 flex justify-between duration-150 
-        hover:border-2 hover:border-white hover-rounded-md last-of-type:rounded-b-xl"
+      className="bg-sky-200 first-of-type:rounded-t-xl border-2 border-sky-200 w-full p-3 flex justify-between duration-150 
+      hover:border-2 hover:border-white hover-rounded-md last-of-type:rounded-b-xl"
+      onClick={() => addItem()}
     >
 
         <p className="text-sky-900 text-lg uppercase">{item.name}</p>
